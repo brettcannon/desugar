9e7fecebd54f4377007aabecbd14768b7d51db1f
Brett Cannon <brett@python.org>
2020-08-10 14:08:55 -0700

    Implement addition

--- desugar/operator.py
+++ desugar/operator.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import typing
+from typing import Callable
 
 from . import builtins as debuiltins
 
@@ -8,27 +9,44 @@ if typing.TYPE_CHECKING:
     from typing import Any
 
 
-def sub(lhs: Any, rhs: Any, /) -> Any:
-    """Implement the binary operation `a - b`."""
-    rhs_type = type(rhs)
-    lhs_type = type(lhs)
-    if rhs_type is not lhs_type and issubclass(rhs_type, lhs_type):
-        call_first = (rhs, rhs_type), "__rsub__", lhs
-        call_second = (lhs, lhs_type), "__sub__", rhs
-    else:
-        call_first = (lhs, lhs_type), "__sub__", rhs
-        call_second = (rhs, rhs_type), "__rsub__", lhs
+def _create_binary_op(name: str, operator: str) -> Callable[[Any, Any], Any]:
+    """Create a binary operation function.
 
-    for first, meth, second_obj in (call_first, call_second):
-        first_obj, first_type = first
-        try:
-            meth = debuiltins._mro_getattr(first_type, meth)
-        except AttributeError:
-            continue
-        value = meth(first_obj, second_obj)
-        if value is not NotImplemented:
-            return value
-    else:
-        raise TypeError(
-            f"unsupported operand type(s) for -: {lhs_type!r} and {rhs_type!r}"
-        )
+    The `name` parameter specifies the name of the special method used for the
+    binary operation (e.g. `sub` for `__sub__`). The `operator` name is the
+    token representing the binary operation (e.g. `-` for subtraction).
+
+    """
+
+    def binary_op(lhs: Any, rhs: Any, /) -> Any:
+        """A closure implementing a binary operation in Python."""
+        rhs_type = type(rhs)
+        lhs_type = type(lhs)
+        if rhs_type is not lhs_type and issubclass(rhs_type, lhs_type):
+            call_first = (rhs, rhs_type), f"__r{name}__", lhs
+            call_second = (lhs, lhs_type), f"__{name}__", rhs
+        else:
+            call_first = (lhs, lhs_type), f"__{name}__", rhs
+            call_second = (rhs, rhs_type), f"__r{name}__", lhs
+
+        for first, meth, second_obj in (call_first, call_second):
+            first_obj, first_type = first
+            try:
+                meth = debuiltins._mro_getattr(first_type, meth)
+            except AttributeError:
+                continue
+            value = meth(first_obj, second_obj)
+            if value is not NotImplemented:
+                return value
+        else:
+            raise TypeError(
+                f"unsupported operand type(s) for {operator}: {lhs_type!r} and {rhs_type!r}"
+            )
+
+    binary_op.__name__ = binary_op.__qualname__ = name
+    binary_op.__doc__ = f"""Implement the binary operation `a {operator} b`."""
+    return binary_op
+
+
+add = _create_binary_op("add", "+")
+sub = _create_binary_op("sub", "-")
--- tests/test_operator.py
+++ tests/test_operator.py
@@ -5,106 +5,142 @@ import pytest
 import desugar.operator
 
 
-class LHSSub:
-    def __sub__(self, other):
+class LHS:
+    def __add__(self, _):
+        return "__add__"
+
+    def __sub__(self, _):
         return "__sub__"
 
 
-class LHSSubNotImplemented:
+class LHSNotImplemented:
     def __init__(self):
         self.called = False
 
-    def __sub__(self, other):
+    def __add__(self, _):
+        self.called = True
+        return NotImplemented
+
+    def __sub__(self, _):
         self.called = True
         return NotImplemented
 
 
-class RHSSub:
-    def __rsub__(self, other):
+class RHS:
+    def __radd__(self, _):
+        return "__radd__"
+
+    def __rsub__(self, _):
         return "__rsub__"
 
 
-class RHSSubNotImplemented:
+class RHSNotImplemented:
     def __init__(self):
         self.rcalled = False
 
-    def __rsub__(self, other):
+    def __radd__(self, _):
+        self.rcalled = True
+        return NotImplemented
+
+    def __rsub__(self, _):
         self.rcalled = True
         return NotImplemented
 
 
-class LHSRHSSub(LHSSub, RHSSub):
+class LHSRHS(LHS, RHS):
     pass
 
 
-class LHSRHSNotImplementedSub(LHSSubNotImplemented, LHSSub, RHSSubNotImplemented):
+class LHSRHSNotImplemented(LHSNotImplemented, LHS, RHSNotImplemented):
     """A subclass for RHS which always returns NotImplemented."""
 
 
-@pytest.mark.parametrize("sub", [operator.sub, desugar.operator.sub])
-class TestSub:
+class BinaryOperationTests:
 
-    """Test desugar.operator.sub()."""
+    """Standard tests for binary operators.
 
-    def test_lhs_called(self, sub):
-        """__sub__ on the LHS when not a subclass is called."""
-        assert sub(LHSSub(), object()) == "__sub__"
+    Subclasses are expected to provide the appropriate details to test a binary
+    operation.
 
-    def test_rhs_called(self, sub):
-        """__rsub__ on the RHS is called when __sub__ is not defined on the LHS."""
-        assert sub(object(), RHSSub()) == "__rsub__"
+    """
 
-    def test_lhs_over_rhs(self, sub):
-        """__sub__ should be called before __rsub__ when RHS is not a subclass of the LHS."""
-        rhs = RHSSubNotImplemented()
-        assert sub(LHSSub(), rhs) == "__sub__"
+    def test_lhs_called(self, op):
+        """__*__ on the LHS when not a subclass is called."""
+        assert op(LHS(), object()) == self.lhs_method
+
+    def test_rhs_called(self, op):
+        """__r*__ on the RHS is called when __*__ is not defined on the LHS."""
+        assert op(object(), RHS()) == self.rhs_method
+
+    def test_lhs_over_rhs(self, op):
+        """__*__ should be called before __r*__ when RHS is not a subclass of the LHS."""
+        rhs = RHSNotImplemented()
+        assert op(LHS(), rhs) == self.lhs_method
         assert not rhs.rcalled
 
-    def test_rhs_subclass_over_lhs(self, sub):
-        """If the RHS is a subclass of LHS then call __rsub__ first."""
-        assert sub(LHSSub(), LHSRHSSub()) == "__rsub__"
+    def test_rhs_subclass_over_lhs(self, op):
+        """If the RHS is a subclass of LHS then call __r*__ first."""
+        assert op(LHS(), LHSRHS()) == self.rhs_method
 
-    def test_both_subclasses(self, sub):
-        """When both the LHS and RHS are of the same type, call LHS.__sub__() first."""
-        assert sub(LHSRHSSub(), LHSRHSSub()) == "__sub__"
+    def test_both_subclasses(self, op):
+        """When both the LHS and RHS are of the same type, call LHS.__*__() first."""
+        assert op(LHSRHS(), LHSRHS()) == self.lhs_method
 
-    def test_rhs_subclass_but_not_implemented(self, sub):
-        """If the RHS is a subclass but returns NotImplemented, call lhs.__sub__()."""
-        lhs = LHSSub()
-        rhs = LHSRHSNotImplementedSub()
-        assert sub(lhs, rhs) == "__sub__"
-        assert not rhs.called
+    def test_rhs_subclass_but_not_implemented(self, op):
+        """If the RHS is a subclass but returns NotImplemented, call lhs.__*__()."""
+        lhs = LHS()
+        rhs = LHSRHSNotImplemented()
+        assert op(lhs, rhs) == self.lhs_method
+        assert not rhs.called  # rhs.__*__() should not be called at any point.
         assert rhs.rcalled
 
-    def test_rhs_not_substituting_for_lhs(self, sub):
-        """If the RHS defines __sub__ but not the LHS, it is still not called."""
+    def test_rhs_not_substituting_for_lhs(self, op):
+        """If the RHS defines __*__ but not the LHS, it is still not called."""
         with pytest.raises(TypeError):
-            sub(object(), LHSSub())
+            op(object(), LHS())
 
-    def test_lhs_not_substituting_for_rhs(self, sub):
-        """If the LHS defines __rsub__ but not the RHS, it is still not called."""
+    def test_lhs_not_substituting_for_rhs(self, op):
+        """If the LHS defines __r*__ but not the RHS, it is still not called."""
         with pytest.raises(TypeError):
-            sub(RHSSub(), object())
+            op(RHS(), object())
 
-    def test_lhs_not_implemented(self, sub):
+    def test_lhs_not_implemented(self, op):
         """If the LHS returns NotImplemented, raise TypeError."""
         with pytest.raises(TypeError):
-            sub(LHSSubNotImplemented(), object())
+            op(LHSNotImplemented(), object())
 
-    def test_rhs_not_implemented(self, sub):
+    def test_rhs_not_implemented(self, op):
         """If the RHS returns NotImplemented, raise TypeError."""
         with pytest.raises(TypeError):
-            sub(object(), RHSSubNotImplemented())
+            op(object(), RHSNotImplemented())
 
-    def test_lhs_not_implemented_but_rhs_is(self, sub):
-        """If lhs.__sub__() returns NotImplemented, call RHS.__rsub__()."""
-        lhs = LHSSubNotImplemented()
-        assert sub(lhs, RHSSub()) == "__rsub__"
+    def test_lhs_not_implemented_but_rhs_is(self, op):
+        """If lhs.__*__() returns NotImplemented, call RHS.__r*__()."""
+        lhs = LHSNotImplemented()
+        assert op(lhs, RHS()) == self.rhs_method
         assert lhs.called
 
-    def test_both_sides_not_implemented(self, sub):
-        """If both lhs.__sub__() and rhs.__rsub__() return NotImplemented, raise TypeError."""
-        lhs = LHSSubNotImplemented()
-        rhs = RHSSubNotImplemented()
+    def test_both_sides_not_implemented(self, op):
+        """If both lhs.__*__() and rhs.__r*__() return NotImplemented, raise TypeError."""
+        lhs = LHSNotImplemented()
+        rhs = RHSNotImplemented()
         with pytest.raises(TypeError):
-            sub(lhs, rhs)
+            op(lhs, rhs)
+
+
+@pytest.mark.parametrize("op", [operator.add, desugar.operator.add])
+class TestAddition(BinaryOperationTests):
+
+    """Tests for desugar.operator.add()."""
+
+    lhs_method = "__add__"
+    rhs_method = "__radd__"
+
+
+@pytest.mark.parametrize("op", [operator.sub, desugar.operator.sub])
+class TestSubtaction(BinaryOperationTests):
+
+    """Tests for desugar.operator.sub()."""
+
+    lhs_method = "__sub__"
+    rhs_method = "__rsub__"
