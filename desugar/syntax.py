8f0c0ce1b0a8a2656b2dd758f822edca41c307a5
Brett Cannon <brett@python.org>
2020-12-15 17:04:18 -0800

    Implement syntax unraveling for augmented assignment

--- desugar/syntax.py
+++ desugar/syntax.py
@@ -1,10 +1,14 @@
+from typing import Callable
+
 import redbaron
 from redbaron import nodes
 
 
-def _create_binary_op_unraveling(op, method):
+def _create_binary_op(
+    op: str, method: str
+) -> Callable[[nodes.BinaryOperatorNode], nodes.AtomtrailersNode]:
     def unravel(node: nodes.BinaryOperatorNode) -> nodes.AtomtrailersNode:
-        """Convert `a + b` to `operator.__add__(a, b)`."""
+        """Convert `a {op} b` to `operator.{method}(a, b)`."""
         left = node.first.dumps()
         right = node.second.dumps()
         top_node = redbaron.RedBaron(f"operator.{method}({left}, {right})")
@@ -15,16 +19,64 @@ def _create_binary_op_unraveling(op, method):
     return unravel
 
 
-unravel_add = _create_binary_op_unraveling("+", "__add__")
-unravel_sub = _create_binary_op_unraveling("-", "__sub__")
-unravel_mul = _create_binary_op_unraveling("*", "__mul__")
-unravel_matmul = _create_binary_op_unraveling("@", "__matmul__")
-unravel_truediv = _create_binary_op_unraveling("/", "__truediv__")
-unravel_floordiv = _create_binary_op_unraveling("//", "__floordiv__")
-unravel_mod = _create_binary_op_unraveling("%", "__mod__")
-unravel_pow = _create_binary_op_unraveling("**", "__pow__")
-unravel_lshift = _create_binary_op_unraveling("<<", "__lshift__")
-unravel_rshift = _create_binary_op_unraveling(">>", "__rshift__")
-unravel_and = _create_binary_op_unraveling("&", "__and__")
-unravel_xor = _create_binary_op_unraveling("^", "__xor__")
-unravel_or = _create_binary_op_unraveling("|", "__or__")
+unravel_add = _create_binary_op("+", "__add__")
+unravel_sub = _create_binary_op("-", "__sub__")
+unravel_mul = _create_binary_op("*", "__mul__")
+unravel_matmul = _create_binary_op("@", "__matmul__")
+unravel_truediv = _create_binary_op("/", "__truediv__")
+unravel_floordiv = _create_binary_op("//", "__floordiv__")
+unravel_mod = _create_binary_op("%", "__mod__")
+unravel_pow = _create_binary_op("**", "__pow__")
+unravel_lshift = _create_binary_op("<<", "__lshift__")
+unravel_rshift = _create_binary_op(">>", "__rshift__")
+unravel_and = _create_binary_op("&", "__and__")
+unravel_xor = _create_binary_op("^", "__xor__")
+unravel_or = _create_binary_op("|", "__or__")
+
+
+def _create_aug_assign(
+    op: str, method: str
+) -> Callable[[nodes.AssignmentNode], nodes.AssignmentNode]:
+    def unravel(node: nodes.AssignmentNode) -> nodes.AssignmentNode:
+        """Convert `a {op}= b` to `a = operator.{method}(a, b)`."""
+        new_node = node.copy()
+        new_node.operator = ""
+        left = node.target.dumps()
+        right = node.value.dumps()
+        lhs = redbaron.RedBaron(f"operator.{method}({left}, {right})")[0]
+        new_node.value = lhs
+        return new_node
+
+    unravel.__name__ = unravel.__qualname__ = f"unravel_{method[2:-2]}"
+    unravel.__doc__ = f"Convert `a {op}= b` to `a = operator.{method}(a, b)`."
+    return unravel
+
+
+unravel_inplace_add = _create_aug_assign("+", "__iadd__")
+unravel_inplace_sub = _create_aug_assign("-", "__isub__")
+unravel_inplace_mul = _create_aug_assign("*", "__imul__")
+unravel_inplace_matmul = _create_aug_assign("@", "__imatmul__")
+unravel_inplace_truediv = _create_aug_assign("/", "__itruediv__")
+unravel_inplace_floordiv = _create_aug_assign("//", "__ifloordiv__")
+unravel_inplace_mod = _create_aug_assign("%", "__imod__")
+unravel_inplace_pow = _create_aug_assign("**", "__ipow__")
+unravel_inplace_lshift = _create_aug_assign("<<", "__ilshift__")
+unravel_inplace_rshift = _create_aug_assign(">>", "__irshift__")
+unravel_inplace_and = _create_aug_assign("&", "__iand__")
+unravel_inplace_xor = _create_aug_assign("^", "__ixor__")
+unravel_inplace_or = _create_aug_assign("|", "__ior__")
+
+# 1. `~ a` ➠ `operator.__invert__(a)`
+# 1. `- a` ➠ `operator.__neg__(a)`
+# 1. `+ a` ➠ `operator.__pos__(a)`
+# 1. `a == b` ➠ `operator.__eq__(a, b)`  (including `object.__eq__()`)
+# 1. `a != b` ➠ `operator.__ne__(a, b)`  (including `object.__ne__()`)
+# 1. `a < b` ➠ `operator.__lt__(a, b)`
+# 1. `a <= b` ➠ `operator.__le__(a, b)`
+# 1. `a > b` ➠ `operator.__gt__(a, b)`
+# 1. `a >= b` ➠ `operator.__ge__(a, b)`
+# 1. `a is b` ➠ `operator.is_(a, b)`
+# 1. `a is not b` ➠ `operator.is_not(a, b)`
+# 1. `not a` ➠ `operator.not_(a)`
+# 1. `a in b` ➠ `operator.__contains__(b, a)`
+# 1. `a not in b` ➠ `operator.not_(operator.__contains__(b, a))`
--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -48,3 +48,50 @@ class TestBinaryArithmetic:
 
     def test_or(self):
         self._test_binary_op("|", "__or__", syntax.unravel_or)
+
+
+class TestAugmentedAssignment:
+    def _test_aug_assign(self, op, method, unravel):
+        given = f"a {op}= b"
+        expect = f"a = operator.{method}(a, b)"
+        result = unravel(redbaron.RedBaron(given)[0])
+        assert result.dumps() == expect
+
+    def test_iadd(self):
+        self._test_aug_assign("+", "__iadd__", syntax.unravel_inplace_add)
+
+    def test_isub(self):
+        self._test_aug_assign("-", "__isub__", syntax.unravel_inplace_sub)
+
+    def test_imul(self):
+        self._test_aug_assign("*", "__imul__", syntax.unravel_inplace_mul)
+
+    def test_imatmul(self):
+        self._test_aug_assign("@", "__imatmul__", syntax.unravel_inplace_matmul)
+
+    def test_itruediv(self):
+        self._test_aug_assign("/", "__itruediv__", syntax.unravel_inplace_truediv)
+
+    def test_ifloordiv(self):
+        self._test_aug_assign("//", "__ifloordiv__", syntax.unravel_inplace_floordiv)
+
+    def test_imod(self):
+        self._test_aug_assign("%", "__imod__", syntax.unravel_inplace_mod)
+
+    def test_ipow(self):
+        self._test_aug_assign("**", "__ipow__", syntax.unravel_inplace_pow)
+
+    def test_ilshift(self):
+        self._test_aug_assign("<<", "__ilshift__", syntax.unravel_inplace_lshift)
+
+    def test_irshift(self):
+        self._test_aug_assign(">>", "__irshift__", syntax.unravel_inplace_rshift)
+
+    def test_iand(self):
+        self._test_aug_assign("&", "__iand__", syntax.unravel_inplace_and)
+
+    def test_ixor(self):
+        self._test_aug_assign("^", "__ixor__", syntax.unravel_inplace_xor)
+
+    def test_ior(self):
+        self._test_aug_assign("|", "__ior__", syntax.unravel_inplace_or)
