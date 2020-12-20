12800a3de669c86a1bdc183eaaff6dbad9a5d748
Brett Cannon <brett@python.org>
2020-12-19 18:24:54 -0800

    Convert syntax for unary operations

--- desugar/syntax.py
+++ desugar/syntax.py
@@ -52,31 +52,104 @@ def _create_aug_assign(
     return unravel
 
 
-unravel_inplace_add = _create_aug_assign("+", "__iadd__")
-unravel_inplace_sub = _create_aug_assign("-", "__isub__")
-unravel_inplace_mul = _create_aug_assign("*", "__imul__")
-unravel_inplace_matmul = _create_aug_assign("@", "__imatmul__")
-unravel_inplace_truediv = _create_aug_assign("/", "__itruediv__")
-unravel_inplace_floordiv = _create_aug_assign("//", "__ifloordiv__")
-unravel_inplace_mod = _create_aug_assign("%", "__imod__")
-unravel_inplace_pow = _create_aug_assign("**", "__ipow__")
-unravel_inplace_lshift = _create_aug_assign("<<", "__ilshift__")
-unravel_inplace_rshift = _create_aug_assign(">>", "__irshift__")
-unravel_inplace_and = _create_aug_assign("&", "__iand__")
-unravel_inplace_xor = _create_aug_assign("^", "__ixor__")
-unravel_inplace_or = _create_aug_assign("|", "__ior__")
+unravel_iadd = _create_aug_assign("+", "__iadd__")
+unravel_isub = _create_aug_assign("-", "__isub__")
+unravel_imul = _create_aug_assign("*", "__imul__")
+unravel_imatmul = _create_aug_assign("@", "__imatmul__")
+unravel_itruediv = _create_aug_assign("/", "__itruediv__")
+unravel_ifloordiv = _create_aug_assign("//", "__ifloordiv__")
+unravel_imod = _create_aug_assign("%", "__imod__")
+unravel_ipow = _create_aug_assign("**", "__ipow__")
+unravel_ilshift = _create_aug_assign("<<", "__ilshift__")
+unravel_irshift = _create_aug_assign(">>", "__irshift__")
+unravel_iand = _create_aug_assign("&", "__iand__")
+unravel_ixor = _create_aug_assign("^", "__ixor__")
+unravel_ior = _create_aug_assign("|", "__ior__")
 
-# 1. `~ a` ➠ `operator.__invert__(a)`
-# 1. `- a` ➠ `operator.__neg__(a)`
-# 1. `+ a` ➠ `operator.__pos__(a)`
+
+def _create_unary_op(
+    op: str, method: str
+) -> Callable[[nodes.UnitaryOperatorNode], nodes.AtomtrailersNode]:
+    def unravel(node: nodes.UnitaryOperatorNode) -> nodes.AtomtrailersNode:
+        target = node.target.dumps()
+        return redbaron.RedBaron(f"operator.{method}({target})")[0]
+
+    unravel.__name__ = unravel.__qualname__ = f"unravel_{method[2:-2]}"
+    unravel.__doc__ = f"Convert `{op} a` to `operator.{method}(a)`."
+    return unravel
+
+
+unravel_invert = _create_unary_op("~", "__invert__")
+unravel_neg = _create_unary_op("-", "__neg__")
+unravel_pos = _create_unary_op("+", "__pos__")
+
 # 1. `a == b` ➠ `operator.__eq__(a, b)`  (including `object.__eq__()`)
 # 1. `a != b` ➠ `operator.__ne__(a, b)`  (including `object.__ne__()`)
 # 1. `a < b` ➠ `operator.__lt__(a, b)`
 # 1. `a <= b` ➠ `operator.__le__(a, b)`
 # 1. `a > b` ➠ `operator.__gt__(a, b)`
 # 1. `a >= b` ➠ `operator.__ge__(a, b)`
+# >>> given = redbaron.RedBaron("a < b"); given[0].help()
+# ComparisonNode()
+#   # identifiers: comparison, comparison_, comparisonnode
+#   first ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='a'
+#   value ->
+#     ComparisonOperatorNode()
+#       # identifiers: comparison_operator, comparison_operator_, comparisonoperator, comparisonoperatornode
+#       first='<'
+#       second=''
+#   second ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='b'
+
 # 1. `a is b` ➠ `operator.is_(a, b)`
 # 1. `a is not b` ➠ `operator.is_not(a, b)`
+# >>> given = redbaron.RedBaron("a is not b"); given[0].help()
+# ComparisonNode()
+#   # identifiers: comparison, comparison_, comparisonnode
+#   first ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='a'
+#   value ->
+#     ComparisonOperatorNode()
+#       # identifiers: comparison_operator, comparison_operator_, comparisonoperator, comparisonoperatornode
+#       first='is'
+#       second='not'
+#   second ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='b'
+
 # 1. `not a` ➠ `operator.not_(a)`
+# >>> given = redbaron.RedBaron("not a"); given[0].help()
+# UnitaryOperatorNode()
+#   # identifiers: unitary_operator, unitary_operator_, unitaryoperator, unitaryoperatornode
+#   value='not'
+#   target ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='a'
+
 # 1. `a in b` ➠ `operator.__contains__(b, a)`
 # 1. `a not in b` ➠ `operator.not_(operator.__contains__(b, a))`
+# >>> given = redbaron.RedBaron("a not in b"); given[0].help()
+# ComparisonNode()
+#   # identifiers: comparison, comparison_, comparisonnode
+#   first ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='a'
+#   value ->
+#     ComparisonOperatorNode()
+#       # identifiers: comparison_operator, comparison_operator_, comparisonoperator, comparisonoperatornode
+#       first='not'
+#       second='in'
+#   second ->
+#     NameNode()
+#       # identifiers: name, name_, namenode
+#       value='b'
--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -58,40 +58,57 @@ class TestAugmentedAssignment:
         assert result.dumps() == expect
 
     def test_iadd(self):
-        self._test_aug_assign("+", "__iadd__", syntax.unravel_inplace_add)
+        self._test_aug_assign("+", "__iadd__", syntax.unravel_iadd)
 
     def test_isub(self):
-        self._test_aug_assign("-", "__isub__", syntax.unravel_inplace_sub)
+        self._test_aug_assign("-", "__isub__", syntax.unravel_isub)
 
     def test_imul(self):
-        self._test_aug_assign("*", "__imul__", syntax.unravel_inplace_mul)
+        self._test_aug_assign("*", "__imul__", syntax.unravel_imul)
 
     def test_imatmul(self):
-        self._test_aug_assign("@", "__imatmul__", syntax.unravel_inplace_matmul)
+        self._test_aug_assign("@", "__imatmul__", syntax.unravel_imatmul)
 
     def test_itruediv(self):
-        self._test_aug_assign("/", "__itruediv__", syntax.unravel_inplace_truediv)
+        self._test_aug_assign("/", "__itruediv__", syntax.unravel_itruediv)
 
     def test_ifloordiv(self):
-        self._test_aug_assign("//", "__ifloordiv__", syntax.unravel_inplace_floordiv)
+        self._test_aug_assign("//", "__ifloordiv__", syntax.unravel_ifloordiv)
 
     def test_imod(self):
-        self._test_aug_assign("%", "__imod__", syntax.unravel_inplace_mod)
+        self._test_aug_assign("%", "__imod__", syntax.unravel_imod)
 
     def test_ipow(self):
-        self._test_aug_assign("**", "__ipow__", syntax.unravel_inplace_pow)
+        self._test_aug_assign("**", "__ipow__", syntax.unravel_ipow)
 
     def test_ilshift(self):
-        self._test_aug_assign("<<", "__ilshift__", syntax.unravel_inplace_lshift)
+        self._test_aug_assign("<<", "__ilshift__", syntax.unravel_ilshift)
 
     def test_irshift(self):
-        self._test_aug_assign(">>", "__irshift__", syntax.unravel_inplace_rshift)
+        self._test_aug_assign(">>", "__irshift__", syntax.unravel_irshift)
 
     def test_iand(self):
-        self._test_aug_assign("&", "__iand__", syntax.unravel_inplace_and)
+        self._test_aug_assign("&", "__iand__", syntax.unravel_iand)
 
     def test_ixor(self):
-        self._test_aug_assign("^", "__ixor__", syntax.unravel_inplace_xor)
+        self._test_aug_assign("^", "__ixor__", syntax.unravel_ixor)
 
     def test_ior(self):
-        self._test_aug_assign("|", "__ior__", syntax.unravel_inplace_or)
+        self._test_aug_assign("|", "__ior__", syntax.unravel_ior)
+
+
+class TestUnaryOp:
+    def _test_unary_op(self, op, method, unravel):
+        given = f"{op} a"
+        expect = f"operator.{method}(a)"
+        result = unravel(redbaron.RedBaron(given)[0])
+        assert result.dumps() == expect
+
+    def test_invert(self):
+        self._test_unary_op("~", "__invert__", syntax.unravel_invert)
+
+    def test_neg(self):
+        self._test_unary_op("-", "__neg__", syntax.unravel_neg)
+
+    def test_pos(self):
+        self._test_unary_op("+", "__pos__", syntax.unravel_pos)
